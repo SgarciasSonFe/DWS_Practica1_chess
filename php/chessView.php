6945e49c32e6cea79f030185674ff57a887fa122
Sergio <repmano04@gmail.com>
2023-10-25 19:05:10 +0200

    draw piezas

--- php/chessView.php
+++ php/chessView.php
@@ -8,41 +8,76 @@
 
 </head>
 <body>
-    <table>
         <?php
+            $board = array(
+                array("RoB","KnB","BiB","QuB","KiB","BiB","KnB","RoB"),
+                array("PaB","PaB","PaB","PaB","PaB","PaB","PaB","PaB"),
+                array(""),
+                array(""),
+                array(""),
+                array(""),
+                array("PaW","PaW","PaW","PaW","PaW","PaW","PaW","PaW"),
+                array("RoW","KnW","BiW","QuW","KiW","BiW","KnW","RoW"));
+
+            DrawChessGame($board);
+        ?>
+
+    <?php
+        function DrawChessGame($board)
+        {
+            echo "<table  class='negro'>";
             for($i=0;$i<8;$i++)
             {
                 echo "<tr>";
-                for($j=0; $j<4; $j++)
+                for($j=0; $j<8; $j++)
                 {
-                    if($i % 2)
+                    if($board[$i][$j] == "RoB")
                     {
-                        echo "<td><div class='negro'></div></td>"; 
-                        echo "<td><div class='blanco'></div></td>";
+                        echo "<td><div class='pieza'><img src='./img/BLArook.png'></div></td>"; 
+                    } else if($board[$i][$j] == "KnB"){
+                        echo "<td><div class='pieza'><img src='./img/BLAknight.png'></div></td>"; 
+                    } else if($board[$i][$j] == "BiB"){
+                        echo "<td><div class='pieza'><img src='./img/BLAbishop.png'></div></td>"; 
+                    } else if($board[$i][$j] == "QuB"){
+                        echo "<td><div class='pieza'><img src='./img/BLAqueen.png'></div></td>"; 
+                    } else if($board[$i][$j] == "KiB"){
+                        echo "<td><div class='pieza'><img src='./img/BLAking.png'></div></td>"; 
+                    } else if($board[$i][$j] == "PaB"){
+                        echo "<td><div class='pieza'><img src='./img/BLApawn.png'></div></td>"; 
+                    } else 
+                    if($board[$i][$j] == "RoW")
+                    {
+                        echo "<td><div class='pieza'><img src='./img/WHIrook.png'></div></td>"; 
+                    } else if($board[$i][$j] == "KnW"){
+                        echo "<td><div class='pieza'><img src='./img/WHIknight.png'></div></td>"; 
+                    } else if($board[$i][$j] == "BiW"){
+                        echo "<td><div class='pieza'><img src='./img/WHIbishop.png'></div></td>"; 
+                    } else if($board[$i][$j] == "QuW"){
+                        echo "<td><div class='pieza'><img src='./img/WHIqueen.png'></div></td>"; 
+                    } else if($board[$i][$j] == "KiW"){
+                        echo "<td><div class='pieza'><img src='./img/WHIking.png'></div></td>"; 
+                    } else if($board[$i][$j] == "PaW"){
+                        echo "<td><div class='pieza'><img src='./img/WHIpawn.png'></div></td>"; 
                     } else {
-                        echo "<td><div class='blanco'></div></td>";
-                        echo "<td><div class='negro'></div></td>"; 
+                        if($i % 2)
+                        {
+                            echo "<td><div class='negro'></div></td>"; 
+                            echo "<td><div class='blanco'></div></td>"; 
+                            $j++;
+                        } else {
+                            echo "<td><div class='blanco'></div></td>"; 
+                            echo "<td><div class='negro'></div></td>"; 
+                            $j++;
+                        }
                     }
                 }
                 echo "</tr>";
             }
-            DrawChessGame(1);
-        ?>
-    </table>
-
-    <?php
-        // $whitePieces = array("<img src='./img/WHIpeon.png'>");
-        // function DrawChessGame($board)
-        // {
-        //     for($i=0;$i<8;$i++)
-        //     {
-        //         for($j=0; $j<4; $j++)
-        //         {
-        //             echo "<div class='pieza'><img src='./img/BLApeon.png'></div>"; 
-        //         }
-        //     }
-        // }
+            echo "</table>";
+                     
+        }
     ?>
+    
     
 </body>
 </html>
