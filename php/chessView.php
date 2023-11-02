3e6f0cff08efcdb193208a6e7b076f1d21bc0c61
Sergio <repmano04@gmail.com>
2023-11-02 16:24:25 +0100

    ChessGame

--- php/chessView.php
+++ php/chessView.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Chess Game</title>
     <link rel="stylesheet" href="chess_game_styles.css">
 
 </head>
@@ -31,8 +31,7 @@
                 echo "<tr>";
                 for($j=0; $j<8; $j++)
                 {
-                    if($board[$i][$j] == "RoB")
-                    {
+                    if($board[$i][$j] == "RoB") {
                         echo "<td><div class='pieza'><img src='./img/BLArook.png'></div></td>"; 
                     } else if($board[$i][$j] == "KnB"){
                         echo "<td><div class='pieza'><img src='./img/BLAknight.png'></div></td>"; 
@@ -44,9 +43,8 @@
                         echo "<td><div class='pieza'><img src='./img/BLAking.png'></div></td>"; 
                     } else if($board[$i][$j] == "PaB"){
                         echo "<td><div class='pieza'><img src='./img/BLApawn.png'></div></td>"; 
-                    } else 
-                    if($board[$i][$j] == "RoW")
-                    {
+                    
+                    } else if($board[$i][$j] == "RoW") {
                         echo "<td><div class='pieza'><img src='./img/WHIrook.png'></div></td>"; 
                     } else if($board[$i][$j] == "KnW"){
                         echo "<td><div class='pieza'><img src='./img/WHIknight.png'></div></td>"; 
@@ -58,7 +56,7 @@
                         echo "<td><div class='pieza'><img src='./img/WHIking.png'></div></td>"; 
                     } else if($board[$i][$j] == "PaW"){
                         echo "<td><div class='pieza'><img src='./img/WHIpawn.png'></div></td>"; 
-                    } 
+                    } else {
                         if($i % 2) 
                         {
                             if($j % 2) 
@@ -76,7 +74,7 @@
                                 echo "<td><div class='blanco'></div></td>";
                             }
                         }
-                    
+                    }
                 }
                 echo "</tr>";
             }
