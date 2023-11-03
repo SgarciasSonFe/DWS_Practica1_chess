6aa2e7a33d19315273f0e99a76872626bec8148a
Sergio <repmano04@gmail.com>
2023-11-03 19:10:49 +0100

    chesView

--- php/chessView.php
+++ php/chessView.php
@@ -18,69 +18,91 @@
                 array(),
                 array("PaW","PaW","PaW","PaW","PaW","PaW","PaW","PaW"),
                 array("RoW","KnW","BiW","QuW","KiW","BiW","KnW","RoW"));
-
+            
             DrawChessGame($board);
         ?>
 
     <?php
         function DrawChessGame($board)
         {
+            $nothing = "";
+            $blaRook = "<img src='./img/BLArook.png'>";
+            $blaKnight = "<img src='./img/BLAknight.png'>";
+            $blaBishop = "<img src='./img/BLAbishop.png'>";
+            $blaQueen = "<img src='./img/BLAqueen.png'>";
+            $blaKing = "<img src='./img/BLAking.png'>";
+            $blaPawn = "<img src='./img/BLApawn.png'>";
+            $whiRook = "<img src='./img/WHIrook.png'>";
+            $whiKnight = "<img src='./img/WHIknight.png'>";
+            $whiBishop = "<img src='./img/WHIbishop.png'>";
+            $whiQueen = "<img src='./img/WHIqueen.png'>";
+            $whiKing = "<img src='./img/WHIking.png'>";
+            $whiPawn = "<img src='./img/WHIpawn.png'>";
+
             echo "<table>";
             for($i=0;$i<8;$i++)
             {
-                echo "<tr>";
-                for($j=0; $j<8; $j++)
+                echo "<tr>"; 
+                for($j=0; $j<8; $j++) 
                 {
                     if($board[$i][$j] == "RoB") {
-                        echo "<td><div class='pieza'><img src='./img/BLArook.png'></div></td>"; 
+                        BoxColor($i,$j,$blaRook);
                     } else if($board[$i][$j] == "KnB"){
-                        echo "<td><div class='pieza'><img src='./img/BLAknight.png'></div></td>"; 
+                        BoxColor($i,$j,$blaKnight);
                     } else if($board[$i][$j] == "BiB"){
-                        echo "<td><div class='pieza'><img src='./img/BLAbishop.png'></div></td>"; 
+                        BoxColor($i,$j,$blaBishop);
                     } else if($board[$i][$j] == "QuB"){
-                        echo "<td><div class='pieza'><img src='./img/BLAqueen.png'></div></td>"; 
+                        BoxColor($i,$j,$blaQueen);
                     } else if($board[$i][$j] == "KiB"){
-                        echo "<td><div class='pieza'><img src='./img/BLAking.png'></div></td>"; 
+                        BoxColor($i,$j,$blaKing);
                     } else if($board[$i][$j] == "PaB"){
-                        echo "<td><div class='pieza'><img src='./img/BLApawn.png'></div></td>"; 
+                        BoxColor($i,$j,$blaPawn); 
                     
                     } else if($board[$i][$j] == "RoW") {
-                        echo "<td><div class='pieza'><img src='./img/WHIrook.png'></div></td>"; 
+                        BoxColor($i,$j,$whiRook); 
                     } else if($board[$i][$j] == "KnW"){
-                        echo "<td><div class='pieza'><img src='./img/WHIknight.png'></div></td>"; 
+                        BoxColor($i,$j,$whiKnight); 
                     } else if($board[$i][$j] == "BiW"){
-                        echo "<td><div class='pieza'><img src='./img/WHIbishop.png'></div></td>"; 
+                        BoxColor($i,$j,$whiBishop); 
                     } else if($board[$i][$j] == "QuW"){
-                        echo "<td><div class='pieza'><img src='./img/WHIqueen.png'></div></td>"; 
+                        BoxColor($i,$j,$whiQueen); 
                     } else if($board[$i][$j] == "KiW"){
-                        echo "<td><div class='pieza'><img src='./img/WHIking.png'></div></td>"; 
+                        BoxColor($i,$j,$whiKing); 
                     } else if($board[$i][$j] == "PaW"){
-                        echo "<td><div class='pieza'><img src='./img/WHIpawn.png'></div></td>"; 
+                        BoxColor($i,$j,$whiPawn); 
                     } else {
-                        if($i % 2) 
-                        {
-                            if($j % 2) 
-                            {
-                                echo "<td><div class='blanco'></div></td>";
-                            } else {
-                                echo "<td><div class='negro'></div></td>";
-                            }
-                        } else if(($i % 2)-1) 
-                        {
-                            if($j % 2) 
-                            {
-                                echo "<td><div class='negro'></div></td>";
-                            } else {
-                                echo "<td><div class='blanco'></div></td>";
-                            }
-                        }
+                        BoxColor($i,$j,$nothing);
                     }
                 }
                 echo "</tr>";
             }
             echo "</table>";
-                     
+
         }
+
+        function BoxColor($i,$j,$pieza)
+        {
+            if($i % 2) 
+            {
+                if($j % 2) 
+                {
+                    echo "<td><div class='blanco'>".$pieza."</div></td>";
+                } else {
+                    echo "<td><div class='negro'>".$pieza."</div></td>";
+                }
+            } else if(($i % 2)-1) 
+            {
+                if($j % 2) 
+                {
+                    echo "<td><div class='negro'>".$pieza."</div></td>";
+                } else {
+                    echo "<td><div class='blanco'>".$pieza."</div></td>";
+                }
+            }
+        }
+
+
+
     ?>
     
     
