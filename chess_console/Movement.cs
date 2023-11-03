167a768954767dd78ced4843f7a0ad389e1d7914
SgarciasSonFe <128164811+SgarciasSonFe@users.noreply.github.com>
2023-11-03 22:35:32 +0100

    Update Movement.cs

--- chess_console/Movement.cs
+++ chess_console/Movement.cs
@@ -21,9 +21,7 @@ namespace ChessAPI
             return _toBoardPosition;
         }
 
-        /// notTODO Practica 02_1
-        /// Ha de validar el rango de los 2 objetos BoardPosition encapsulados
-        /// en esta clase.
+       
         public bool IsValid()
         {
             if(BoardPosition.ValidateBoardPosition(_fromBoardPosition) && BoardPosition.ValidateBoardPosition(_toBoardPosition))
